91d238a0676f732317762cd5d2dbdf55fb55504f
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-08-04 18:13:15 +0530

    feat: initialize Drizzle ORM schema

---
+++ sandbox/schema.ts
@@ -0,0 +1,9 @@
+import { primaryKey } from "drizzle-orm/cockroach-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).unique().notNull(),
+});
