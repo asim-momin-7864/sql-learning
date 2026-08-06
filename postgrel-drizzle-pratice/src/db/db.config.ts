ad6c84e1e938c6d322f1db513e9b6871a40c2549
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-08-06 22:09:22 +0530

    feat: Config database

---
+++ postgrel-drizzle-pratice/src/db/db.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { relations } from "./relations";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const db = drizzle({
+  client: pool,
+  relations: relations,
+});
+
+export default db;
